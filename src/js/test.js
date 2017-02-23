41e9db213f3926cdc54a91e2ec1590e7b2df0c27
frank <yangjunalns@qq.com>
2017-02-23 19:12:27 +0800

    页面的完善

--- src/js/test.js
+++ src/js/test.js
@@ -3,8 +3,12 @@ $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
 
+$(function () {
+  $('[data-toggle="popover"]').popover()
+})
+
 function copyText() {
-    var clipboard = new Clipboard('#btn-copy');
+    var clipboard = new Clipboard('.btn-copy');
     clipboard.on('success', function(e) {
         console.info('Action:', e.action);
         e.clearSelection();
